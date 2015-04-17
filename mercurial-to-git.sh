ab1bf9639cce00aac9ad7a8eed8eb3f9bf32b289
Ron Kurr <kurr@kurron.org>
2015-04-16 21:53:01 -0400

    Final version

--- mercurial-to-git.sh
+++ mercurial-to-git.sh
@@ -8,6 +8,5 @@ for dir in $SOURCE/*
     do 
         echo converting $dir
         cd $dir ; hg archive $TARGET/$dir ; cd -
-        git init $TARGET/$dir
-        cd $TARGET/dir ; git add --all ; git commit -a -m 'Import from Mercurial' ; cd -
+        cd $TARGET/$dir ; git init ; git add --all ; git commit -a -m 'Import from Mercurial' ; cd -
     done
