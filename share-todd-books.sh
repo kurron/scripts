9487134cab3d96ab89481c1dba8eb9efcac13f50
Ron Kurr <kurr@kurron.org>
2015-05-05 17:01:31 -0400

    Flattened the epub files

--- share-todd-books.sh
+++ share-todd-books.sh
@@ -13,16 +13,18 @@ echo 'Rsyncing from G drive to ForMobile'
 #mkdir /cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd 
 
 echo 'Sharing PDF books...'
-#mkdir -p /cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/pdf
+mkdir -p /cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/pdf
 mkdir -p /tmp/Todd/pdf
+rm -rf /tmp/ronbo/pdf
 mkdir -p /tmp/ronbo/pdf
 rsync --verbose --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.pdf' --exclude='*' '/cygdrive/g/Users/TLougee/Books/' '/tmp/Todd/pdf'
 find /tmp/Todd/pdf -type f -name \*.pdf -exec cp --verbose `basename {}` /tmp/ronbo/pdf \;
 rsync --verbose --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.pdf' --exclude='*' '/tmp/ronbo/pdf/' '/cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/pdf'
 
 echo 'Sharing epub books...'
-#mkdir -p /cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/epub
+mkdir -p /cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/epub
 mkdir -p /tmp/Todd/epub
+rm -rf /tmp/ronbo/epub
 mkdir -p /tmp/ronbo/epub
 rsync --verbose --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.epub' --exclude='*' '/cygdrive/g/Users/TLougee/Books/' '/tmp/Todd/epub'
 find /tmp/Todd/epub -type f -name \*.epub -exec cp --verbose `basename {}` /tmp/ronbo/epub \;
