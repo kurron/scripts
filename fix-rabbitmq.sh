65ca842f1d272b2d95f2f94bef57c83c65f4ede6
Ron Kurr <kurr@kurron.org>
2015-06-19 13:19:55 +0000

    Included the management console

--- fix-rabbitmq.sh
+++ fix-rabbitmq.sh
@@ -3,4 +3,4 @@
 # remember to stop and remove these containers by hand first
 
 docker run --detach --name rabbitmq-data --volume /var/lib/rabbitmq busybox
-docker run --detach --name rabbitmq --hostname rabbitmq --publish 5672:5672 --publish 15672:15672 --volumes-from rabbitmq-data --restart always rabbitmq
+docker run --detach --name rabbitmq --hostname rabbitmq --publish 5672:5672 --publish 15672:15672 --volumes-from rabbitmq-data --restart always rabbitmq:management
