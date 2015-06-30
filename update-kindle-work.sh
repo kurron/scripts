3974ccfa23ec2b7734530e4f16792eb066766a29
Ron Kurr <kurr@kurron.org>
2015-06-30 19:36:50 -0400

    Summarize changes

--- update-kindle-work.sh
+++ update-kindle-work.sh
@@ -4,4 +4,4 @@ FROM=/cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/mobi/
 TO=/cygdrive/e/documents/transparent-language
 
 echo 'Copying mobi books...'
-rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.mobi' --exclude='*' $FROM $TO
+rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='*.mobi' --exclude='*' $FROM $TO
