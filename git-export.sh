88bc43382cf0d5bfae49ae457c4417321391b759
Ron Kurr <kurr@kurron.org>
2015-07-13 12:59:07 +0000

    Added Git export script

---
+++ git-export.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# you must be in the Git project you wish to export
+BRANCH_TO_EXPORT=$1
+DESTINATION=$2
+mkdir -p $DESTINATION
+git archive $BRANCH_TO_EXPORT | tar -x -C $DESTINATION 
