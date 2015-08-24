7e1cb081a8b47cd2557372fdc93728324e5eeb85
Ron Kurr <kurr@kurron.org>
2015-08-24 17:38:28 +0000

    Enabled user authentication

--- fix-mongodb.sh
+++ fix-mongodb.sh
@@ -3,4 +3,4 @@
 # remember to stop and remove these containers by hand first
 
 docker run --detach --name mongodb-data --volume /data/db busybox
-docker run --detach --name mongodb --hostname mongodb --publish 27017:27017 --volumes-from mongodb-data --restart always mongo --storageEngine=wiredTiger --notablescan --journalCommitInterval=300 --directoryperdb
+docker run --detach --name mongodb --hostname mongodb --publish 27017:27017 --volumes-from mongodb-data --restart always mongo --storageEngine=wiredTiger --notablescan --journalCommitInterval=300 --directoryperdb --auth
