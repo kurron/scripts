bdd6eb44142aa033671e1469aad2def686fddd22
Ron Kurr <kurr@kurron.org>
2015-08-24 20:15:33 +0000

    Added notes on how to add global accounts

--- fix-mongodb.sh
+++ fix-mongodb.sh
@@ -2,5 +2,11 @@
 
 # remember to stop and remove these containers by hand first
 
+# use admin
+# db.createUser( { user: "admin", pwd: "admin", roles: ["root"] } )
+# db.auth( "admin", "admin" )
+# db.createUser( { user: "readWrite", pwd: "readWrite", roles: [ "readWriteAnyDatabase" ] } )
+# db.createUser( { user: "read", pwd: "read", roles: [ "readAnyDatabase" ] } )
+
 docker run --detach --name mongodb-data --volume /data/db busybox
 docker run --detach --name mongodb --hostname mongodb --publish 27017:27017 --volumes-from mongodb-data --restart always mongo --storageEngine=wiredTiger --notablescan --journalCommitInterval=300 --directoryperdb --auth
