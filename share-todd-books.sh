d4e6571b443aafc6a7f54303cfd79da2cd3a4518
Ron Kurr <kurr@kurron.org>
2016-01-25 09:28:54 -0500

    Share the Packt Publishing books

--- share-todd-books.sh
+++ share-todd-books.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-ROOT=/cygdrive/i/BitTorrenSync/Todd
-
 echo 'Sharing PDF books...'
-#mkdir -p /cygdrive/i/BitTorrenSync/Todd/pdf
-#mkdir -p /tmp/Todd/pdf
-#rm -rf /tmp/ronbo/pdf
-#mkdir -p /tmp/ronbo/pdf
-#rsync --verbose --recursive  --times --checksum --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.pdf' --exclude='*' '/cygdrive/g/Users/TLougee/Books/' '/tmp/Todd/pdf'
-#find /tmp/Todd/pdf -type f -name \*.pdf -exec cp --verbose $(basename {}) /tmp/ronbo/pdf \;
-#rsync --verbose --checksum --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.pdf' --exclude='*' '/tmp/ronbo/pdf/' '/cygdrive/i/BitTorrenSync/Todd/pdf'
+mkdir -p /cygdrive/i/BitTorrenSync/Todd/pdf
+mkdir -p /tmp/Todd/pdf
+rm -rf /tmp/ronbo/pdf
+mkdir -p /tmp/ronbo/pdf
+rsync --verbose --recursive  --times --checksum --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.pdf' --exclude='*' '/cygdrive/g/Users/TLougee/Books/Packet Publishing/' '/tmp/Todd/pdf'
+find /tmp/Todd/pdf -type f -name \*.pdf -exec cp --verbose $(basename {}) /tmp/ronbo/pdf \;
+rsync --verbose --checksum --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.pdf' --exclude='*' '/tmp/ronbo/pdf/' '/cygdrive/i/BitTorrenSync/Todd/pdf'
 
 echo 'Sharing epub books...'
-#mkdir -p /cygdrive/i/BitTorrenSync/Todd/epub
-#mkdir -p /tmp/Todd/epub
-#rm -rf /tmp/ronbo/epub
-#mkdir -p /tmp/ronbo/epub
-#rsync --verbose --recursive  --times --delete --checksum --prune-empty-dirs --human-readable --progress --include='*/' --include='*.epub' --exclude='*' '/cygdrive/g/Users/TLougee/Books/' '/tmp/Todd/epub'
-#find /tmp/Todd/epub -type f -name \*.epub -exec cp --verbose $(basename {}) /tmp/ronbo/epub \;
-#rsync --verbose --checksum --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.epub' --exclude='*' '/tmp/ronbo/epub/' '/cygdrive/i/BitTorrenSync/Todd/epub'
+mkdir -p /cygdrive/i/BitTorrenSync/Todd/epub
+mkdir -p /tmp/Todd/epub
+rm -rf /tmp/ronbo/epub
+mkdir -p /tmp/ronbo/epub
+rsync --verbose --recursive  --times --delete --checksum --prune-empty-dirs --human-readable --progress --include='*/' --include='*.epub' --exclude='*' '/cygdrive/g/Users/TLougee/Books/Packet Publishing/' '/tmp/Todd/epub'
+find /tmp/Todd/epub -type f -name \*.epub -exec cp --verbose $(basename {}) /tmp/ronbo/epub \;
+rsync --verbose --checksum --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.epub' --exclude='*' '/tmp/ronbo/epub/' '/cygdrive/i/BitTorrenSync/Todd/epub'
 
 echo 'Sharing mobi books...'
-#mkdir -p /cygdrive/i/BitTorrenSync/Todd/mobi
-#mkdir -p /tmp/Todd/mobi
-#rm -rf /tmp/ronbo/mobi
-#mkdir -p /tmp/ronbo/mobi
-#rsync --verbose --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.mobi' --exclude='*' '/cygdrive/g/Users/TLougee/Books/' '/tmp/Todd/mobi'
-#find /tmp/Todd/mobi -type f -name \*.mobi -exec cp --verbose $(basename {}) /tmp/ronbo/mobi \;
-#rsync --verbose --checksum --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.mobi' --exclude='*' '/tmp/ronbo/mobi/' '/cygdrive/i/BitTorrenSync/Todd/mobi'
+mkdir -p /cygdrive/i/BitTorrenSync/Todd/mobi
+mkdir -p /tmp/Todd/mobi
+rm -rf /tmp/ronbo/mobi
+mkdir -p /tmp/ronbo/mobi
+rsync --verbose --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.mobi' --exclude='*' '/cygdrive/g/Users/TLougee/Books/Packet Publishing/' '/tmp/Todd/mobi'
+find /tmp/Todd/mobi -type f -name \*.mobi -exec cp --verbose $(basename {}) /tmp/ronbo/mobi \;
+rsync --verbose --checksum --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.mobi' --exclude='*' '/tmp/ronbo/mobi/' '/cygdrive/i/BitTorrenSync/Todd/mobi'
 
 echo 'Sharing archive files...'
 #mkdir -p /cygdrive/i/BitTorrenSync/Todd/zip
