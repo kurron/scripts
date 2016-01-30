6b35aa18e60e7dce76a0a94545c3aff08a32ef3f
Ron Kurr <kurr@kurron.org>
2016-01-30 13:16:51 -0500

    Do not copy the .sync folder

--- update-kindle-work-dropbox.sh
+++ update-kindle-work-dropbox.sh
@@ -5,4 +5,4 @@ FROM=/cygdrive/i/BitTorrenSync/'Todd Books'/Apps/
 TO=/cygdrive/e/documents/transparent-language
 
 echo 'Copying mobi books...'
-rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='*.mobi' --exclude='*' "$FROM" $TO
+rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --exclude='.sync/' --include='*/' --include='*.mobi' --exclude='*' "$FROM" $TO
