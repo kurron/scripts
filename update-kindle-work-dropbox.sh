c58ef0ccbf7e209f0e873fce973db77601d89e2b
Ron Kurr <kurr@kurron.org>
2016-01-30 13:55:42 -0500

    Also include Packt publishing books

--- update-kindle-work-dropbox.sh
+++ update-kindle-work-dropbox.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #FROM=/cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/mobi/
-FROM=/cygdrive/i/BitTorrenSync/'Todd Books'/Apps/
+#FROM=/cygdrive/i/BitTorrenSync/'Todd Books'/Apps/
+FROM=/cygdrive/i/BitTorrenSync/
 TO=/cygdrive/e/documents/transparent-language
 
 echo 'Copying mobi books...'
-rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --exclude='.sync/' --include='*/' --include='*.mobi' --exclude='*' "$FROM" $TO
+rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --exclude='Todd Books/Apps/.sync/' --include='*/' --include='*.mobi' --exclude='*' "$FROM" $TO
