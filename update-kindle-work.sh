0b43579f3531173f2a843aac50a1b4b97c2d332e
Ron Kurr <kurr@kurron.org>
2016-03-21 18:28:38 -0400

    Exclude Packet Publishing

--- update-kindle-work.sh
+++ update-kindle-work.sh
@@ -6,4 +6,4 @@ FROM='/cygdrive/i/BitTorrenSync/Todd Books/Apps/'
 TO=/cygdrive/e/documents/transparent-language
 
 echo "Copying mobi books from $FROM to $TO"
-rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='*.mobi' --exclude='*' "$FROM" "$TO"
+rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --exclude='Packet Publishing/'  --include='*/' --include='*.mobi' --exclude='*' "$FROM" "$TO"
