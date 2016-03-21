503c5cf154d4b442b600c6e6cf7b7fc87387a350
Ron Kurr <kurr@kurron.org>
2016-03-21 12:53:21 -0400

    Improved the script a bit

--- update-kindle-work.sh
+++ update-kindle-work.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #FROM=/cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/mobi/
-FROM=/cygdrive/i/BitTorrenSync/Todd/mobi/
+#FROM=/cygdrive/i/BitTorrenSync/Todd/mobi/
+FROM='/cygdrive/i/BitTorrenSync/Todd Books/Apps/'
 TO=/cygdrive/e/documents/transparent-language
 
-echo 'Copying mobi books...'
-rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='*.mobi' --exclude='*' $FROM $TO
+echo "Copying mobi books from $FROM to $TO"
+rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='*.mobi' --exclude='*' "$FROM" "$TO"
