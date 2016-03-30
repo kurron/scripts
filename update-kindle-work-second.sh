f04fc35e5bd92d2ffbe48f7fe93da66b9ea737d4
Ron Kurr <kurr@kurron.org>
2016-03-30 16:23:01 -0400

    Works with new sync folder

--- update-kindle-work-second.sh
+++ update-kindle-work-second.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 #FROM=/cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/mobi/
-FROM=/cygdrive/i/BitTorrenSync/Todd/mobi/
+#FROM=/cygdrive/i/BitTorrenSync/Todd/mobi/
+FROM=/cygdrive/i/BitTorrenSync/Todd\ Books/Apps/
 TO=/cygdrive/e/documents/transparent-language
 
-echo 'Copying mobi books...'
-rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='[M-Z]*.mobi' --exclude='*' $FROM $TO
+CMD="rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='[M-Z]*.mobi' --exclude='*' '$FROM' '$TO'"
+
+echo eval $CMD
+eval $CMD
