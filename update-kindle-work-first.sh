d145d35bb2edf2f3b0c85875b4429166b049a922
Ron Kurr <kurr@kurron.org>
2016-03-30 13:48:08 -0400

    Fixed the script so it works with the new sync area

--- update-kindle-work-first.sh
+++ update-kindle-work-first.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #FROM=/cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/mobi/
-FROM=/cygdrive/i/BitTorrenSync/Todd/mobi/
+#FROM=/cygdrive/i/BitTorrenSync/Todd/mobi/
+FROM=/cygdrive/i/BitTorrenSync/Todd\ Books/Apps/
 TO=/cygdrive/e/documents/transparent-language
 
-echo 'Copying mobi books...'
-rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='[0-L]*.mobi' --exclude='*' $FROM $TO
+CMD="rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='[0-L]*.mobi' --exclude='*' '$FROM' '$TO'"
+echo eval $CMD
+eval $CMD
