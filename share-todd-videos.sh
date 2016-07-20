6333352d14e35832c5f593b2c55838b345c1db3b
Ron Kurr <kurr@kurron.org>
2016-07-20 15:28:48 -0400

    Use the new torrent location

--- share-todd-videos.sh
+++ share-todd-videos.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo 'Sharing video files...'
-mkdir -p /cygdrive/i/BitTorrenSync/Todd/video
-rsync --verbose --checksum --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.mp4' --exclude='*' '/cygdrive/g/Users/TLougee/Books/' '/cygdrive/i/BitTorrenSync/Todd/video'
+rsync --verbose --checksum --recursive  --times --delete --prune-empty-dirs --human-readable --progress --include='*/' --include='*.mp4' --exclude='*' '/cygdrive/g/Users/TLougee/Books/' '/cygdrive/d/BitTorrent Sync/Todd/video'
