5587385ab8527cf1ed9c3f182bb49cfd396fa6b9
Ron Kurr <kurr@kurron.org>
2016-07-25 12:40:46 -0400

    Exclude the Packt Publishing books for now

--- update-kindle-work-second.sh
+++ update-kindle-work-second.sh
@@ -6,7 +6,7 @@
 FROM=/cygdrive/d/BitTorrent\ Sync/Apps/
 TO=/cygdrive/e/documents/transparent-language
 
-CMD="rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='[H-Zh-z]*.mobi' --exclude='*' '$FROM' '$TO'"
+CMD="rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --exclude='Packet Publishing/' --exclude='Free and Not Updated O?Reilly Books/' --include='*/' --include='[M-Zm-z]*.mobi' --exclude='*' '$FROM' '$TO'"
 
 echo eval $CMD
 eval $CMD
