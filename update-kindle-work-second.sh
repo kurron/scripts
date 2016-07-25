ae54a1a6f6b53ef478ea7ec8234f426b47561b01
Ron Kurr <kurr@kurron.org>
2016-07-25 11:27:40 -0400

    Sync from work computer

--- update-kindle-work-second.sh
+++ update-kindle-work-second.sh
@@ -2,10 +2,11 @@
 
 #FROM=/cygdrive/c/cygwin64/home/RKurr/BitTorrentSync/Todd/mobi/
 #FROM=/cygdrive/i/BitTorrenSync/Todd/mobi/
-FROM=/cygdrive/i/BitTorrenSync/Todd\ Books/Apps/
+#FROM=/cygdrive/i/BitTorrenSync/Todd\ Books/Apps/
+FROM=/cygdrive/d/BitTorrent\ Sync/Apps/
 TO=/cygdrive/e/documents/transparent-language
 
-CMD="rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='[M-Zm-z]*.mobi' --exclude='*' '$FROM' '$TO'"
+CMD="rsync --verbose --recursive --checksum --delete --prune-empty-dirs --human-readable --progress --itemize-changes --include='*/' --include='[H-Zh-z]*.mobi' --exclude='*' '$FROM' '$TO'"
 
 echo eval $CMD
 eval $CMD
