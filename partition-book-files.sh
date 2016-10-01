f58a2bdf9f270b1a67105fb84c22d9a33f5d78aa
Ron Kurr <kurr@kurron.org>
2016-10-01 11:07:21 -0400

    Use Mobi

--- partition-book-files.sh
+++ partition-book-files.sh
@@ -8,13 +8,13 @@ LAST_HALF=$TO/last
 rm -rf ${TO}
 mkdir -p ${TO}
 
-# copy the epub files into a folder
-FIND_ONE="find ${FROM} -type f -name '*.epub' -exec cp --verbose {} ${TO} \;"
+# copy the mobi files into a folder
+FIND_ONE="find ${FROM} -type f -name '*.mobi' -exec cp --verbose {} ${TO} \;"
 #echo eval ${FIND_ONE}
 eval ${FIND_ONE}
 
 # figure out how many files we are dealing with
-FIND_TWO="find ${TO} -type f -name '*.epub' | wc -l"
+FIND_TWO="find ${TO} -type f -name '*.mobi' | wc -l"
 echo eval ${FIND_TWO}
 
 COUNT=$(eval ${FIND_TWO})
@@ -27,11 +27,11 @@ rm -rf ${FIRST_HALF}
 mkdir -p ${FIRST_HALF}
 
 # copy the first half to its folder
-find ${TO} -type f -name '*.epub' | sort | head --lines ${HALF} | sed 's/.*/"&"/' | xargs cp --verbose  --target-directory ${FIRST_HALF}
+find ${TO} -type f -name '*.mobi' | sort | head --lines ${HALF} | sed 's/.*/"&"/' | xargs cp --verbose  --target-directory ${FIRST_HALF}
 
 
 rm -rf ${LAST_HALF}
 mkdir -p ${LAST_HALF}
 
 # copy the last half to its folder
-find ${TO} -type f -name '*.epub' | sort | tail --lines ${HALF} | sed 's/.*/"&"/' | xargs cp --verbose  --target-directory ${LAST_HALF}
+find ${TO} -type f -name '*.mobi' | sort | tail --lines ${HALF} | sed 's/.*/"&"/' | xargs cp --verbose  --target-directory ${LAST_HALF}
