20dfd5297929b14582d2906e0db82b0abd64c583
Ron Kurr <kurr@kurron.org>
2016-10-01 09:21:14 -0400

    Do math

--- go.sh
+++ go.sh
@@ -3,9 +3,22 @@
 FROM="/cygdrive/d/BitTorrent\ Sync/Apps/"
 TO=/tmp/ronbo
 
-rm -rf ${TO}
-mkdir -p ${TO}
+#rm -rf ${TO}
+#mkdir -p ${TO}
 
 FIND_ONE="find ${FROM} -type f -name '*.epub' -exec cp --verbose {} ${TO} \;"
-echo eval ${FIND_ONE}
-eval ${FIND_ONE}
+#echo eval ${FIND_ONE}
+#eval ${FIND_ONE}
+
+FIND_TWO="find ${TO} -type f -name '*.epub' | wc -l"
+echo eval ${FIND_TWO}
+
+COUNT=$(eval ${FIND_TWO})
+echo $COUNT
+
+HALF=$((COUNT/2))
+echo $HALF
+
+FIND_THREE="find ${TO} -type f -name '*.epub' | sort | limit "
+echo eval ${FIND_TWO}
+
