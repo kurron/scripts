a27a5df7fa93ec85a8b6490209e90748e27e219f
Ron Kurr <kurr@kurron.org>
2016-10-15 11:25:27 -0400

    Need sshpass if you want to connect using passwords

--- install-ansible-fedora.sh
+++ install-ansible-fedora.sh
@@ -5,7 +5,7 @@
 until sudo dnf -y update; do echo "Waiting for repository lock"; sleep 5; done
 
 # supposedly, this is the newer way to install pip
-sudo dnf -y install python-pip python-devel libffi-devel openssl-devel gcc redhat-rpm-config
+sudo dnf -y install python-pip python-devel libffi-devel openssl-devel gcc redhat-rpm-config sshpass
 sudo pip install --upgrade pip
 sudo pip install --upgrade paramiko
 sudo pip install --upgrade ansible
