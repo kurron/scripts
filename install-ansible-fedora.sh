e7668e00d041727831df77f80cba1e6de16dc901
Ron Kurr <kurr@kurron.org>
2016-10-15 10:22:50 -0400

    Install Ansible on Fedora

---
+++ install-ansible-fedora.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#!/bin/bash
+
+until sudo dnf -y update; do echo "Waiting for repository lock"; sleep 5; done
+
+# supposedly, this is the newer way to install pip
+sudo dnf -y install python-pip python-devel libffi-devel openssl-devel gcc redhat-rpm-config
+sudo pip install --upgrade pip
+sudo pip install --upgrade paramiko
+sudo pip install --upgrade ansible
+sudo pip install --upgrade boto
