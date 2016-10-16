468aebc805b618c2fb67a09a6634b3587419b551
Ron Kurr <kurr@kurron.org>
2016-10-16 17:24:43 -0400

    Install missing Python packages that Ansible needs

--- install-ansible-fedora.sh
+++ install-ansible-fedora.sh
@@ -3,7 +3,7 @@
 until sudo dnf -y update; do echo "Waiting for repository lock"; sleep 5; done
 
 # supposedly, this is the newer way to install pip
-sudo dnf -y install python-pip python-devel libffi-devel openssl-devel gcc redhat-rpm-config sshpass
+sudo dnf -y install python-pip python-devel libffi-devel openssl-devel gcc redhat-rpm-config sshpass python2 python2-dnf libselinux-python
 sudo pip install --upgrade pip
 sudo pip install --upgrade paramiko
 sudo pip install --upgrade ansible
