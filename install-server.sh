e9bbbe80b92b7823be0a50b1a7aa2e12f01799d7
Ron Kurr <kurr@kurron.org>
2016-11-12 12:03:59 -0500

    Support stargate

--- install-server.sh
+++ install-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass server.yml
+#ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit @/home/rkurr/GitHub/scripts/server.retry server.yml
