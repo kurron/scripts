bc0d2abe6bed09b1b306606063cb8e630b38d89d
Ron Kurr <kurr@kurron.org>
2016-11-12 16:59:13 -0500

    Install VirtualBox on boxes that can handle it

--- install-server.sh
+++ install-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass server.yml
+#ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass server.yml
 #ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit @/home/rkurr/GitHub/scripts/server.retry server.yml
+ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit stargate server.yml
