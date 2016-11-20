d89265ba02f47aba9a2e586ac909435007541f28
Ron Kurr <kurr@kurron.org>
2016-11-20 10:26:55 -0500

    Updated vanaheim

--- install-server.sh
+++ install-server.sh
@@ -2,4 +2,5 @@
 
 #ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass server.yml
 #ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit @/home/rkurr/GitHub/scripts/server.retry server.yml
-ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit atlantis --tags backup server.yml
+#ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit atlantis --tags backup server.yml
+ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit vanaheim server.yml
