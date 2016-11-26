6e2fffcd420bd694f008366a8d3793a0068f24b3
Ron Kurr <kurr@kurron.org>
2016-11-26 17:30:56 -0500

    Have servers ping Loggly on the hour

--- install-server.sh
+++ install-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass server.yml
+ansible-playbook --verbose --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass server.yml
 #ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit @/home/rkurr/GitHub/scripts/server.retry server.yml
 #ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit atlantis --tags backup server.yml
-ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit vanaheim server.yml
+#ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit vanaheim server.yml
