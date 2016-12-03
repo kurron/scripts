5c3d7c3aa24ab8e2a29bdb65bf51479c3bfab862
Ron Kurr <kurr@kurron.org>
2016-12-02 20:35:14 -0500

    Do not restrict installation to just Hashicorp

--- install-desktop.sh
+++ install-desktop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#ansible-playbook --connection=local --user rkurr --ask-pass --become-user root --ask-become-pass desktop.yml
-ansible-playbook --connection=local --user rkurr --ask-pass --become-user root --ask-become-pass --tags hashicorp desktop.yml
+ansible-playbook --connection=local --user rkurr --ask-pass --become-user root --ask-become-pass desktop.yml
+#ansible-playbook --connection=local --user rkurr --ask-pass --become-user root --ask-become-pass --tags hashicorp desktop.yml
