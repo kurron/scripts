ccad56d7bacdb405966ba4a9919ff032ebd91c0e
Ron Kurr <kurr@kurron.org>
2017-01-01 10:58:45 -0500

    Add tags to roles to make it easier to selectively run plays

--- install-server.sh
+++ install-server.sh
@@ -3,4 +3,5 @@
 #ansible-playbook --verbose --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass server.yml
 #ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit @/home/rkurr/GitHub/scripts/server.retry server.yml
 #ansible-playbook --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit atlantis --tags backup server.yml
-ansible-playbook --verbose --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit asgard --tags virtualbox server.yml
+#ansible-playbook --verbose --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --limit asgard --tags virtualbox server.yml
+ansible-playbook --verbose --inventory=servers --user rkurr --ask-pass --become-user root --ask-become-pass --tags resilio server.yml
