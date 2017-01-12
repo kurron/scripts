7ca66737b0167ec64457e037971eaa7bf4445475
Ron Kurr <kurr@kurron.org>
2017-01-12 12:50:54 -0500

    Added Graphetti Monkey support

--- collect-facts.sh
+++ collect-facts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible --inventory "54.202.197.1," --user ec2-user --become-user root --private-key /home/vagrant/Bitbucket/Operations/aws-ssh-keys/us-west-2/asgard-lite-test.pem all -m setup
+ansible --inventory "54.244.207.92," --user ec2-user --become-user root --private-key /home/vagrant/Bitbucket/Operations/aws-ssh-keys/us-west-2/asgard-lite-test.pem all -m setup
---
+++ tag-from-ec2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://github.com/Answers4AWS/graffiti-monkey
+# sudo pip install graffiti_monkey
+
+#graffiti-monkey --dryrun --region us-west-2 --config graffiti-monkey.yml
+graffiti-monkey --region us-west-2 --config graffiti-monkey.yml
