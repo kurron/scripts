80fe971638272f1691ad5e095589e297eba74c05
Ron Kurr <kurr@kurron.org>
2017-02-25 09:42:44 -0500

    Do not bother trying to describe the China region because you will be denied.

--- list-all-availability-zones.sh
+++ list-all-availability-zones.sh
@@ -2,9 +2,10 @@
  
 rm zones.txt
 
-for region in us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1 eu-west-1 eu-west-2 eu-central-1 ap-northeast-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-south-1 sa-east-1 cn-north-1
+for region in us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1 eu-west-1 eu-west-2 eu-central-1 ap-northeast-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-south-1 sa-east-1
 do
    CMD="aws ec2 --region ${region} describe-availability-zones"
+   echo ${CMD}
    ${CMD} >> zones.txt
 done
 
