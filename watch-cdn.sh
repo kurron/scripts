c4fa594bc9524a9c1df5bfcb5f0d61a81c56c26b
Ron Kurr <kurr@kurron.org>
2017-02-25 09:14:36 -0500

    Simple script to exercise the CDN

---
+++ watch-cdn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CDN=${1:-d1phq4yrkrmw68.cloudfront.net}
+ELB=${2:-internal-Phoen-LoadB-ZMUPOXFQ3RGN-477287101.us-west-2.elb.amazonaws.com}
+DELAY=${3:-1}
+
+CMD="http --verbose --follow https://${CDN}/alpha/ elb==${ELB} port==80 endpoint==/bravo/"
+
+for (( ; ; )) 
+do
+#  echo ${CMD}
+   ${CMD}
+   sleep ${DELAY} 
+done
+
