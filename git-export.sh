131eabb8a65b4043c0b59703d942d5970d05400e
Ron Kurr <kurr@kurron.org>
2017-03-18 15:17:59 -0400

    Added some convenient defaults

--- git-export.sh
+++ git-export.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -ex
+
 # you must be in the Git project you wish to export
-BRANCH_TO_EXPORT=$1
-DESTINATION=$2
-mkdir -p $DESTINATION
-git archive $BRANCH_TO_EXPORT | tar --extract --directory $DESTINATION
+BRANCH_TO_EXPORT=${1:-master}
+DESTINATION=${2:-/tmp/bob}
+
+mkdir --parents --verbose ${DESTINATION}
+git archive ${BRANCH_TO_EXPORT} | tar --extract --directory ${DESTINATION}
+
+echo Your newly exported project can be found in ${DESTINATION}
