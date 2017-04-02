a3524df09ff0fa93696a0390c7855648ff6506eb
Ron Kurr <kurr@kurron.org>
2017-04-02 17:13:30 -0400

    Can corrupts packets now

--- corrupt-packets.sh
+++ corrupt-packets.sh
@@ -3,14 +3,15 @@
 # https://wiki.linuxfoundation.org/networking/netem
 
 INTERFACE=${1:-enp0s8}
-BASE=${2:-10.0%}
+BASE=${2:-50.0%}
+VARIANCE=${3:-10%}
 
 # clear out existing entries
 RESET="sudo tc qdisc del dev ${INTERFACE} root"
 echo ${RESET}
 ${RESET}
 
-# introduce random packet corruption
-CORRUPT="sudo tc qdisc add dev ${INTERFACE} root netem corrupt ${BASE}"
-echo ${CORRUPT}
-${CORRUPT}
+# introduced packet loss that varies
+DROP="sudo tc qdisc add dev ${INTERFACE} root netem loss ${BASE} ${VARIANCE}"
+echo ${DROP}
+${DROP}
