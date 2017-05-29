f350fb3de750b5432476563e856d24350ccc948b
Ron Kurr <kurr@kurron.org>
2017-05-29 13:02:59 -0400

    Create restricted accounts for student experimentation

---
+++ create-student.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#set -ex
+
+sudo groupadd --force starwars
+
+# password is starwars
+
+for i in luke leia han chewbacca kylo rey finn poe  
+do
+   echo "$i"
+   sudo useradd --comment ${i} --password '$6$NGvwZMsd$E5D1ETakhf/y14kZ/t7W6ywwFJYbYHaVgzDaLUtFfMBP4yRc7feJzShQMtKKXXQag5jq6fe856QzRCR5g8QDS1' --create-home --user-group --groups starwars ${i}
+done
+
+sudo mkdir /home/shared-folder
+sudo chown -R luke:starwars /home/shared-folder
+ls -l --all --human /home
