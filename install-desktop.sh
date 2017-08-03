7fe777fe76cba81117de3fec809fde1a8a1b4bea
Ron Kurr <kurr@kurron.org>
2017-08-02 20:22:40 -0400

    I forget why I did this

--- install-desktop.sh
+++ install-desktop.sh
@@ -3,5 +3,5 @@
 # clean up the old binaries so the new ones can replace them
 sudo rm -f /usr/local/bin/packer
 
-ansible-playbook --connection=local --user rkurr --ask-pass --become-user root --ask-become-pass desktop.yml
-#ansible-playbook --connection=local --user rkurr --ask-pass --become-user root --ask-become-pass --tags hashicorp desktop.yml
+#ansible-playbook --connection=local --user rkurr --ask-pass --become-user root --ask-become-pass desktop.yml
+ansible-playbook --connection=local --user rkurr --ask-pass --become-user root --ask-become-pass --tags hashicorp desktop.yml
