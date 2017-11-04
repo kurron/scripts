ffd8bb3b09b95e49f354f4c8972d382a3212ed84
Ron Kurr <kurr@kurron.org>
2017-11-04 11:09:07 -0400

    Added entries for other endpoints

--- curl-slurp-e.sh
+++ curl-slurp-e.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# development
 #curl --silent --header "x-api-key:CHs9qbXE9naZIeQynhJPA1ejzbshJpGr8LLwJfcz" https://government.transparent.engineering/development/slurp-e-api
-curl --verbose --header "x-api-key:P1oSNp226J4sIgKGYrhvV4VjpxarreUJj0miDmjf" https://government.transparent.engineering/test/slurp-e-api
+
+# test
+
+#curl --silent --header "x-api-key:P1oSNp226J4sIgKGYrhvV4VjpxarreUJj0miDmjf" https://government.transparent.engineering/test/slurp-e-api
+#curl --silent --header "x-api-key:P1oSNp226J4sIgKGYrhvV4VjpxarreUJj0miDmjf" https://government.transparent.engineering/test/slurp-e-journaler/operations/health
+ curl --silent --header "x-api-key:P1oSNp226J4sIgKGYrhvV4VjpxarreUJj0miDmjf" https://government.transparent.engineering/test/slurp-e-processor/operations/health
