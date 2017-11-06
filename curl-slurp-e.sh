06103e7f2b1f71926b9898f8561a4eda6c170732
Ron Kurr <kurr@kurron.org>
2017-11-06 13:42:40 -0500

    Add links via the ALB

--- curl-slurp-e.sh
+++ curl-slurp-e.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# development
-#curl --silent --header "x-api-key:CHs9qbXE9naZIeQynhJPA1ejzbshJpGr8LLwJfcz" https://government.transparent.engineering/development/slurp-e-api
+# development AGW
+3curl --silent --header "x-api-key:CHs9qbXE9naZIeQynhJPA1ejzbshJpGr8LLwJfcz" https://government.transparent.engineering/development/slurp-e-api
 
-# test
+# test AGW
 
 #curl --silent --header "x-api-key:P1oSNp226J4sIgKGYrhvV4VjpxarreUJj0miDmjf" https://government.transparent.engineering/test/slurp-e-api
 #curl --silent --header "x-api-key:P1oSNp226J4sIgKGYrhvV4VjpxarreUJj0miDmjf" https://government.transparent.engineering/test/slurp-e-journaler/operations/health
- curl --silent --header "x-api-key:P1oSNp226J4sIgKGYrhvV4VjpxarreUJj0miDmjf" https://government.transparent.engineering/test/slurp-e-processor/operations/health
+#curl --silent --header "x-api-key:P1oSNp226J4sIgKGYrhvV4VjpxarreUJj0miDmjf" https://government.transparent.engineering/test/slurp-e-processor/operations/health
+
+# development ALB
+#curl --silent  http://alb-20171024152039476400000002-1770168985.us-east-1.elb.amazonaws.com:2070/operations/health
+#curl --silent  http://alb-20171024152039476400000002-1770168985.us-east-1.elb.amazonaws.com:2080/operations/health
+curl --silent  http://alb-20171024152039476400000002-1770168985.us-east-1.elb.amazonaws.com:2090/development/slurp-e-api/operations/health
