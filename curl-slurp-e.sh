9524db8b5dc345af1a758e793904c3a2b6d69659
Ron Kurr <kurr@kurron.org>
2017-11-28 10:18:07 -0500

    Fix stupid typo

--- curl-slurp-e.sh
+++ curl-slurp-e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # development AGW
-3curl --silent --header "x-api-key:CHs9qbXE9naZIeQynhJPA1ejzbshJpGr8LLwJfcz" https://government.transparent.engineering/development/slurp-e-api
+curl --silent --header "x-api-key:CHs9qbXE9naZIeQynhJPA1ejzbshJpGr8LLwJfcz" https://government.transparent.engineering/development/slurp-e-api
 
 # test AGW
 
