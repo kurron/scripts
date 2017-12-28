542c22e0e03f7b93bd3f43ad220f5b50e384c7dc
Ron Kurr <kurr@kurron.org>
2017-12-27 19:15:20 -0500

    Use the --set-home like pip suggested

--- install-ansible-ubuntu.sh
+++ install-ansible-ubuntu.sh
@@ -2,9 +2,9 @@
 
 until sudo apt -y update; do echo "Waiting for apt lock"; sleep 5; done
 
-sudo apt install -y python-pip python-dev build-essential libssl-dev libffi-dev apt-transport-https sshpass
-sudo pip install --upgrade pip
-sudo pip install --upgrade ansible
-sudo pip install --upgrade setuptools
+sudo --set-home apt install -y python-pip python-dev build-essential libssl-dev libffi-dev apt-transport-https sshpass
+sudo --set-home pip install --upgrade pip
+sudo --set-home pip install --upgrade ansible
+sudo --set-home pip install --upgrade setuptools
 
 # to test: ansible all -i "localhost," --user rkurr --ask-pass --become-user rkurr --become --ask-become-pass -m setup | less
