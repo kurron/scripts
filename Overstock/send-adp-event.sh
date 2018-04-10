095bb788d15053910803dd22c12a1e9d98c72cc1
Ron Kurr <kurr@kurron.org>
2018-04-10 17:15:33 -0400

    Can populate all known attribtes. The ancillary strucuture remains null.

--- Overstock/send-adp-event.sh
+++ Overstock/send-adp-event.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 BODY=${1:-./adp-event.json}
-CMD="http --verify no --json POST https://nkkmfsfq10.execute-api.us-west-1.amazonaws.com/adpevents/ @${BODY}"
+#CMD="http --verify no --json POST https://nkkmfsfq10.execute-api.us-west-1.amazonaws.com/adpevents/ @${BODY}"
+#CMD="http --verify no --json POST https://nkkmfsfq10.execute-api.us-west-1.amazonaws.com/adpevents/ chnl=some-chnl eid=some-eid aid=some-aid pos=some-pos sid=some-sid etype=some-etype uid=some-uid tid=some-tid edate=some-edate chnltype=some-chnltype viewable=some-viewable elm_id=some-elm_id etime=some-etime heid=some-heid"
+CMD="http --verify no --json POST https://nkkmfsfq10.execute-api.us-west-1.amazonaws.com/adpevents/ \
+     eid=some-eid \
+     chnl=some-chnl \
+     chnltype=some-chnltype \
+     etype=some-etype \
+     aid=some-aid \
+     uid=some-uid \
+     heid=some-heid \
+     sid=some-sid \
+     pos=1234 \
+     viewable=true \
+     elmid=5678 \
+     tid=some-tid \
+     ronbo=Logan"
 echo ${CMD}
 ${CMD}
