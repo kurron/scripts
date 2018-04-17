73133312045a8918512d785ccb04401afb741ebf
Ron Kurr <kurr@kurron.org>
2018-04-17 11:28:39 -0400

    The API changed a little

--- Overstock/by-asset-id.sh
+++ Overstock/by-asset-id.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ID=${1:-f2ae455a5b32145496dfa9c317d391ed}
-CMD="http --verify no https://www.orfeo.io/assets/${ID}"
+CMD="http --verbose --verify no https://www.orfeo.io/assets/id/${ID}"
 ${CMD}
