b577d151a84176e9ff45b6807ef87e62f3a4117c
Ron Kurr <kurr@jvmguy.com>
2018-05-15 11:46:01 -0400

    Add Kibana to the mix

--- Overstock/create-adp-containers.sh
+++ Overstock/create-adp-containers.sh
@@ -2,15 +2,21 @@
 
 # this script creates the necessary containers for integration testing
 
-docker pull docker.elastic.co/elasticsearch/elasticsearch:6.0.1
-docker run --name elastic-test -p 9200:9200 -p 9300:9300 -d -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.0.1
+docker pull docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.4
+docker run --name elastic-test --hostname elasticsearch -p 9200:9200 -p 9300:9300 -d -e "discovery.type=single-node" --volume /tmp:/usr/share/elasticsearch/data docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.4
+echo Wating for Elasticsearch to fully come up....
 sleep 45s
 curl localhost:9200/_cat/health
 /home/vagrant/Bitbucket/Orfeo/adp-dbcore/docker/elastic/elastic-search-setup.sh
 
 docker pull scylladb/scylla:2.1.1
-docker run --name some-scylla --volume /home/vagrant/Bitbucket/Orfeo/adp-dbcore/docker/scylla:/opt/resources -p 9042:9042 -d scylladb/scylla:2.1.1
+docker run --name some-scylla --hostname scylladb --volume /home/vagrant/Bitbucket/Orfeo/adp-dbcore/docker/scylla:/opt/resources -p 9042:9042 -d scylladb/scylla:2.1.1
 sleep 15s
 docker exec -it some-scylla nodetool status
 docker exec some-scylla cqlsh -f /opt/resources/scylladb-setup.cql
 docker exec -it some-scylla cqlsh -e 'DESC SCHEMA;'
+
+docker pull docker.elastic.co/kibana/kibana-oss:6.2.4
+docker run --name kibana-test --hostname kibana -p 5601:5601 -d -e "ELASTICSEARCH_URL=http://172.17.0.1:9200/" docker.elastic.co/kibana/kibana-oss:6.2.4 
+docker logs --follow kibana-test
+
