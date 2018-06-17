3ec46e03dd6d8d0431390f1367884735a7bc6ab1
Ron Kurr <kurr@kurron.org>
2018-06-17 10:45:01 -0400

    Docker installation script.

---
+++ install-docker.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+ sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+docker --version
+
+docker run hello-world
