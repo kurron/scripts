26b286ab7f3b23b24b865ae6c0eda7389a1bbbb6
Ron Kurr <kurr@jvmguy.com>
2018-08-27 14:17:39 +0000

    Make thinks a little clearer.

--- Overstock/by-asset-id-resized.sh
+++ Overstock/by-asset-id-resized.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 ID=${1:-fad27d57a0820d7f25e830e4fa01b61e}
-CMD="http --verbose --verify no --follow alb-2018072720411595490000001e-1254405108.us-east-2.elb.amazonaws.com/assets/id/${ID}/resized?width=240&height=160"
-#CMD="http --verbose --verify no --follow https://www.orfeo.io/assets/id/${ID}/resized?width=1024&height=768"
+
+# staging account
+CMD="http --verbose --follow alb-2018072720411595490000001e-1254405108.us-east-2.elb.amazonaws.com/assets/id/${ID}/resized?width=2048&height=768"
+
+# marketing account
+#CMD="http --verbose --verify no --follow --print Hh https://www.orfeo.io/assets/id/${ID}/resized?width=2048&height=768"
 echo ${CMD}
 ${CMD}
