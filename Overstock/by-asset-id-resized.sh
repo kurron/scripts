27ad7035bf0330a2ae1b2c98293556f5a4ba227c
Ron Kurr <kurr@jvmguy.com>
2018-09-11 17:15:41 +0000

    Support firendly DSN name.

--- Overstock/by-asset-id-resized.sh
+++ Overstock/by-asset-id-resized.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-ID=${1:-8ef7410016aa80f4c3b33e3620fd41ca}
+ID=${1:-0333860bf62c3480c4460d51103dcb22}
+KEY=${2:-2Rv5FfKDb96FFgeSPA4mO2Y6mBCtdA4paKp1JeNf}
 
 # staging account
-CMD="http --verbose --follow alb-2018072720411595490000001e-1254405108.us-east-2.elb.amazonaws.com/assets/id/${ID}/resized?width=1024&height=768"
+#CMD="http --verbose --follow alb-2018072720411595490000001e-1254405108.us-east-2.elb.amazonaws.com/assets/id/${ID}/resized?width=1024&height=768"
+#CMD="http --verbose --follow https://o4l052orob.execute-api.us-east-2.amazonaws.com/adp-api-generated-staging/assets/id/${ID}/resized?width=1024&height=768 x-api-key:${KEY}"
+CMD="http --verbose --follow https://staging-adp-api.orfeo.io/adp-api-generated-staging/assets/id/${ID}/resized?width=1024&height=768 x-api-key:${KEY}"
 
 # marketing account
 #CMD="http --verbose --verify no --follow --print Hh https://www.orfeo.io/assets/id/${ID}/resized?width=2048&height=768"
