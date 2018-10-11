39ddd065e8c8a0e5c1233b671bed3698ebaefef7
Ron Kurr <kurr@jvmguy.com>
2018-10-11 16:20:23 +0000

    Made the script more general.

--- Overstock/by-asset-id-resized.sh
+++ Overstock/by-asset-id-resized.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-ID=${1:-0333860bf62c3480c4460d51103dcb22}
-KEY=${2:-2Rv5FfKDb96FFgeSPA4mO2Y6mBCtdA4paKp1JeNf}
+ID=${1:-0002f37bfea9498d9e390077ffbc0438}
+KEY=${2:-rTdrLnz6xr1Jm8o0xXdyt76U2vipYw5F4NdkfZz2}
+ENVIRONMENT=${3:-staging}
 
-# staging account
-#CMD="http --verbose --follow alb-2018072720411595490000001e-1254405108.us-east-2.elb.amazonaws.com/assets/id/${ID}/resized?width=1024&height=768"
-#CMD="http --verbose --follow https://o4l052orob.execute-api.us-east-2.amazonaws.com/adp-api-generated-staging/assets/id/${ID}/resized?width=1024&height=768 x-api-key:${KEY}"
-CMD="http --verbose --follow https://staging-adp-api.orfeo.io/adp-api-generated-staging/assets/id/${ID}/resized?width=1024&height=768 x-api-key:${KEY}"
+CMD="http --verbose --follow https://${ENVIRONMENT}-adp-api.orfeo.io/${ENVIRONMENT}/assets/id/${ID}/resized?width=1024&height=768 x-api-key:${KEY}"
 
 # marketing account
-#CMD="http --verbose --verify no --follow --print Hh https://www.orfeo.io/assets/id/${ID}/resized?width=2048&height=768"
+#CMD="http --verbose --verify no --follow --print Hh https:///assets/id/${ID}/resized?width=2048&height=768"
 echo ${CMD}
 ${CMD}
