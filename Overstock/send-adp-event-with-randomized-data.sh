6c8c50548bbee9c333b16c25d7a914167e6a2b8c
Ron Kurr <kurr@jvmguy.com>
2018-12-03 21:51:07 +0000

    Reference the gateway instead of the load balancer.

--- Overstock/send-adp-event-with-randomized-data.sh
+++ Overstock/send-adp-event-with-randomized-data.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
+URL=${1-https://development-adp-events.orfeo.io}
+
 R=${RANDOM}
 B=$((RANDOM%2))
 CMD="http --verify no \
 	  --json POST \
-          https://staging-adp-events.orfeo.io/staging chnl=${R} \
-	                                                                    eid=${R} \
-									    aid=${R} \
-									    pos=${R} \
-									    sid=${R} \
-									    etype=${R} \
-									    uid=${R} \
-									    tid=${R} \
-									    chnltype=${R} \
-									    viewable=${B} \
-									    elmid=${R} \
-									    heid=${R} \
-									    rid=${R}"
+          ${URL} chnl=${R} \
+	         eid=${R} \
+		 aid=${R} \
+		 pos=${R} \
+		 sid=${R} \
+		 etype=${R} \
+		 uid=${R} \
+		 tid=${R} \
+		 chnltype=${R} \
+		 viewable=${B} \
+		 elmid=${R} \
+		 heid=${R} \
+		 rid=${R}"
 echo ${CMD}
 ${CMD}
 
