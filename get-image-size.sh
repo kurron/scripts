6c32feb4536b6976bd90ea2233216249519d8efb
Ron Kurr <kurr@jvmguy.com>
2018-12-12 20:16:17 +0000

    Better output

--- get-image-size.sh
+++ get-image-size.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-echo "Determining image dimensions..."
 FILE=${1}
 CMD="identify -format %[width]x%[height] ${FILE}"
 #echo ${CMD}
 DIMENSIONS=$(${CMD})
-echo "${FILE} is ${DIMENSIONS}"
+echo "${FILE} dimensions are ${DIMENSIONS}"
 
