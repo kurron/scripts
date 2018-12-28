cbd20c4d7f7f09896a978f7f9e5af1b97527b067
Ron Kurr <kurr@jvmguy.com>
2018-12-28 17:06:54 +0000

    Tweaked some scripts.

--- Overstock/by-asset-id.sh
+++ Overstock/by-asset-id.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ID=${1:-d4c502d66bc7e603aab1581d7e792d78}
-CMD="http --verbose --verify no alb-2018072720411595490000001e-1254405108.us-east-2.elb.amazonaws.com/assets/id/${ID}"
+CMD="http --verbose localhost:9090/assets/id/${ID}"
 ${CMD}
--- Overstock/by-product-id.sh
+++ Overstock/by-product-id.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-ID=${1:-13807640}
-CMD="http --verify no https://www.orfeo.io/assets/product/${ID}"
+ID=${1:-12015208}
+BODY=query.json
+#CMD="http https://production-adp-api.orfeo.io/assets/product/${ID}?limit=5&sort_field:image_width&sort_mode:MEDIAN x-api-key:Kiw2vnGfb43iovVYUQFhq7ENA3BDzXbT2XtoPCpJ"
+CMD="http POST https://production-adp-api.orfeo.io/assets/search/?limit=5 x-api-key:Kiw2vnGfb43iovVYUQFhq7ENA3BDzXbT2XtoPCpJ @${BODY}"
+echo ${CMD}
 ${CMD}
