94227ac4cccfa4873c0e8d930debf906e69156da
Ron Kurr <kurr@kurron.org>
2019-01-05 11:44:52 -0500

    Upgrade to MakeMKV 1.14.2

--- install-make-mkv.sh
+++ install-make-mkv.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-BINARY=${1:-http://www.makemkv.com/download/makemkv-bin-1.12.3.tar.gz}
-SOURCE=${2:-http://www.makemkv.com/download/makemkv-oss-1.12.3.tar.gz}
+VERSION=1.14.2
+BINARY="http://www.makemkv.com/download/makemkv-bin-${VERSION}.tar.gz"
+SOURCE="http://www.makemkv.com/download/makemkv-oss-${VERSION}.tar.gz"
 
 PACKAGES="sudo apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev zlib1g-dev curl"
 
@@ -17,5 +18,5 @@ ${GET_BINARY}
 echo ${GET_SOURCE}
 ${GET_SOURCE}
 
-cd /tmp && tar zxvf /tmp/makemkv-oss.tar.gz && cd /tmp/makemkv-oss-1.12.3 ; ./configure ; make ; sudo make install
-cd /tmp && tar zxvf /tmp/makemkv-bin.tar.gz && cd /tmp/makemkv-bin-1.12.3 ; make ; sudo make install
+cd /tmp && tar zxvf /tmp/makemkv-oss.tar.gz && cd /tmp/makemkv-oss-${VERSION} ; ./configure ; make ; sudo make install
+cd /tmp && tar zxvf /tmp/makemkv-bin.tar.gz && cd /tmp/makemkv-bin-${VERSION} ; make ; sudo make install
