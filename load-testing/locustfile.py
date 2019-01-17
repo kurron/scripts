db348cf6f99e0ca5e49f56d59d474507c90c640c
Ron Kurr <kurr@jvmguy.com>
2019-01-17 15:40:18 +0000

    Look into the response document.

--- load-testing/locustfile.py
+++ load-testing/locustfile.py
@@ -4,7 +4,13 @@ class SimpleLocustTest(TaskSet):
 
     @task
     def get_something(self):
-        self.client.get("/assets/id/855dc1586b0cb9a07d7afa83bb769576")
+        with self.client.get("/assets/id/855dc1586b0cb9a07d7afa83bb769576", catch_response=True) as response:
+#            print("Response status code:", response.status_code)
+#            print("Response content:", response.text)
+            json = response.json()
+ #           print( json["header"]["code"] )
+            if json["header"]["code"] != 0:
+                response.failure("Error code is non-zero: " + json["header"]["code"] )
 
 class LocustTests(HttpLocust):
     task_set = SimpleLocustTest
