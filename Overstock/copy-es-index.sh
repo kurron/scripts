bad797b9467933a6a29e076085b84de34d8edae6
Ron Kurr <kurr@jvmguy.com>
2019-02-19 21:41:33 +0000

    Added a convenience script to copy one ES index to another

---
+++ Overstock/copy-es-index.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Copy one Elasticsearch index to another: https://www.elastic.co/guide/en/elasticsearch/reference/6.2/docs-reindex.html
+# use something like http localhost:9200/_tasks/XYX4ftXJRk-uaVojW1sKWA:12969867 to track the status of the reindex
+
+PORT=${1:-9200}
+CMD="curl --request POST \
+	  --header Content-Type:application/json \
+          --data @reindex.json  \
+	  http://localhost:${PORT}/_reindex?wait_for_completion=false"
+echo ${CMD}
+${CMD}
