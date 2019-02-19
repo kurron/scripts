2119c1f3fb2443405eab983270fcf0f0f55775de
Ron Kurr <kurr@jvmguy.com>
2019-02-19 21:50:25 +0000

    Added cancelling command

--- Overstock/copy-es-index.sh
+++ Overstock/copy-es-index.sh
@@ -2,6 +2,7 @@
 
 # Copy one Elasticsearch index to another: https://www.elastic.co/guide/en/elasticsearch/reference/6.2/docs-reindex.html
 # use something like http localhost:9200/_tasks/XYX4ftXJRk-uaVojW1sKWA:12969867 to track the status of the reindex
+# to cancel use curl -X POST localhost:9200/_tasks/XYX4ftXJRk-uaVojW1sKWA:12969867/_cancel
 
 PORT=${1:-9200}
 CMD="curl --request POST \
