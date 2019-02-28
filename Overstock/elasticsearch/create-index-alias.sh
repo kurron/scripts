e6c606eca1ce6d6029f0ac849666f65458170802
Ron Kurr <kurr@jvmguy.com>
2019-02-28 19:52:26 +0000

    Try not to use data files.

--- Overstock/elasticsearch/create-index-alias.sh
+++ Overstock/elasticsearch/create-index-alias.sh
@@ -3,9 +3,12 @@
 # Create a stable name for an index: https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-aliases.html
 
 PORT=${1:-9200}
+DATA='{ "actions" : [ { "add" : { "index" : "assets8", "alias" : "adp-data" } } ] }'
 CMD="curl --request POST \
 	  --header Content-Type:application/json \
-          --data @alias.json  \
+          --data '${DATA}'  \
 	  http://localhost:${PORT}/_aliases"
 echo ${CMD}
-${CMD}
+#${CMD}
+
+
