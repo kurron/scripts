c47dea1252092efe107256f1c07c9823f00c057e
Ron Kurr <kurr@jvmguy.com>
2019-03-04 21:07:42 +0000

    Create aliases for both indexes.

--- Overstock/elasticsearch/create-index-alias.sh
+++ Overstock/elasticsearch/create-index-alias.sh
@@ -3,12 +3,10 @@
 # Create a stable name for an index: https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-aliases.html
 
 PORT=${1:-9200}
-DATA='{ "actions" : [ { "add" : { "index" : "assets8", "alias" : "adp-data" } } ] }'
 CMD="curl --request POST \
 	  --header Content-Type:application/json \
-          --data '${DATA}'  \
+          --data @alias.json  \
 	  http://localhost:${PORT}/_aliases"
 echo ${CMD}
-#${CMD}
-
+${CMD}
 
