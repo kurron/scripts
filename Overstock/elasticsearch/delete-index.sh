25a74a5d83d8609f38202e7a26234636380bb9e9
Ron Kurr <kurr@jvmguy.com>
2019-03-05 20:44:23 +0000

    Default to the old index.

--- Overstock/elasticsearch/delete-index.sh
+++ Overstock/elasticsearch/delete-index.sh
@@ -5,7 +5,7 @@
 # to cancel use curl -X POST localhost:9200/_tasks/XYX4ftXJRk-uaVojW1sKWA:12969867/_cancel
 
 PORT=${1:-9200}
-INDEX=${2:-assets8}
+INDEX=${2:-assets7}
 CMD="http DELETE http://localhost:${PORT}/${INDEX}"
 echo ${CMD}
 ${CMD}
