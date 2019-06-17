b0bcce253cef3a7be9ed11392c9f70f24f564697
Ron Kurr <kurr@jvmguy.com>
2019-06-17 16:19:06 +0000

    Improved index and alias support.

--- Overstock/elasticsearch/move-index-alias.sh
+++ Overstock/elasticsearch/move-index-alias.sh
@@ -3,9 +3,10 @@
 # Create a stable name for an index: https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-aliases.html
 
 PORT=${1:-9200}
+DEFINITION=${2:-move-alias.json}
 CMD="curl --request POST \
 	  --header Content-Type:application/json \
-          --data @move-alias.json  \
+          --data @${DEFINITION}  \
 	  http://localhost:${PORT}/_aliases"
 echo ${CMD}
 ${CMD}
