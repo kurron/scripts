00fcd7d0e45e751777bdd5b9cbb1ff607c3b4525
Ron Kurr <kurr@kurron.org>
2019-08-25 16:36:47 -0400

    Added brave support

---
+++ install-brave.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Brave from source repository 
+
+PROGNAME=$(basename $0)
+error_exit()
+{
+          echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+          exit 1
+}
+
+sudo apt install apt-transport-https curl || error_exit "Unable to install necessary programs."
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - || error_exit "Unable to install repository key."
+source /etc/os-release || error_exit "Unable to determine OS."
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list || error_exit "Unable to repository."
+sudo apt update || error_exit "Unable to determine OS."
+sudo apt install brave-browser || error_exit "Unable to install browser."
+
