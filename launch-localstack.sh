6c3247178f694a9a8f5a8827656d0c462bae4af2
Ron Kurr <kurr@jvmguy.com>
2019-09-20 19:46:10 +0000

    Launch Localstack via Docker command-line.

--- launch-localstack.sh
+++ launch-localstack.sh
@@ -7,10 +7,29 @@ error_exit()
 	  exit 1
 }
 
+USER_ID=$(id -u $(whoami))
+GROUP_ID=$(id -g $(whoami))
+
 echo Updating Base Image...
 docker pull localstack/localstack:latest || error_exit "Unable to pull Docker image."
 
-export TMPDIR=/home/vagrant/Localstack
 echo Launching localstack
-localstack start --docker || error_exit "Unable to start localstack."
 
+#export TMPDIR=/home/vagrant/Localstack
+#xport TMPDIR=/tmp
+#export DOCKER_FLAGS='-e ELASTICSEARCH_PORT="9200"'
+#localstack start --docker || error_exit "Unable to start localstack."
+
+COMMAND="docker run \
+		 --env-file localstack.ini \
+		 --interactive \
+		 --name localstack \
+		 --network host \
+		 --privileged \
+		 --rm \
+		 --tty \
+		 --volume /tmp:/tmp/localstack \
+		 --volume /var/run/docker.sock:/var/run/docker.sock \
+         localstack/localstack:latest"
+echo ${COMMAND}
+${COMMAND} || error_exit "Unable to start localstack."
