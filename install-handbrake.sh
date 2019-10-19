4488783004c94a0d76da6e4ec28550b31938668c
Ron Kurr <kurr@kurron.org>
2019-10-19 17:48:14 -0400

    Improve error handling.

--- install-handbrake.sh
+++ install-handbrake.sh
@@ -2,11 +2,18 @@
 
 # https://www.ubuntupit.com/handbrake-a-free-and-open-source-transcoder-for-linux/ 
 
+PROGNAME=$(basename $0)
+error_exit()
+{
+          echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+          exit 1
+}
+
 echo Installing Handbrake
 
-sudo add-apt-repository ppa:stebbins/handbrake-releases
+sudo add-apt-repository ppa:stebbins/handbrake-releases || error_exit "Unable to add repository."
 
-sudo apt-get update
+sudo apt-get update || error_exit "Unable to update packages."
 
-sudo apt-get install handbrake-gtk handbrake-cli
+sudo apt-get install handbrake-gtk handbrake-cli || error_exit "Unable to install packages."
 
