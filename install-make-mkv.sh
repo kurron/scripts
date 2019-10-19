df73a56a713b8609b51fc60c024ba94020aab032
Ron Kurr <kurr@kurron.org>
2019-10-19 17:44:37 -0400

    Improve error handling.

--- install-make-mkv.sh
+++ install-make-mkv.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+PROGNAME=$(basename $0)
+error_exit()
+{
+          echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+          exit 1
+}
+
 VERSION=1.14.5
 BINARY="http://www.makemkv.com/download/makemkv-bin-${VERSION}.tar.gz"
 SOURCE="http://www.makemkv.com/download/makemkv-oss-${VERSION}.tar.gz"
@@ -7,16 +14,16 @@ SOURCE="http://www.makemkv.com/download/makemkv-oss-${VERSION}.tar.gz"
 PACKAGES="sudo apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev zlib1g-dev curl"
 
 echo ${PACKAGES}
-${PACKAGES}
+${PACKAGES} || error_exit "Unable to install required packages."
 
-GET_BINARY="curl --output /tmp/makemkv-bin.tar.gz ${BINARY}"
+GET_BINARY="curl --output /tmp/makemkv-bin.tar.gz ${BINARY}" 
 GET_SOURCE="curl --output /tmp/makemkv-oss.tar.gz ${SOURCE}"
 
 echo ${GET_BINARY}
-${GET_BINARY}
+${GET_BINARY}  || error_exit "Unable to get binaries."
 
 echo ${GET_SOURCE}
-${GET_SOURCE}
+${GET_SOURCE}  || error_exit "Unable to install source files."
 
-cd /tmp && tar zxvf /tmp/makemkv-oss.tar.gz && cd /tmp/makemkv-oss-${VERSION} ; ./configure ; make ; sudo make install
-cd /tmp && tar zxvf /tmp/makemkv-bin.tar.gz && cd /tmp/makemkv-bin-${VERSION} ; make ; sudo make install
+cd /tmp && tar zxvf /tmp/makemkv-oss.tar.gz && cd /tmp/makemkv-oss-${VERSION} && ./configure && make && sudo make install
+cd /tmp && tar zxvf /tmp/makemkv-bin.tar.gz && cd /tmp/makemkv-bin-${VERSION} && make && sudo make install
