46379df03ea43f4a03585a4889a9f3ae3fb4f377
Ron Kurr <kurr@jvmguy.com>
2019-12-11 14:54:44 +0000

    Added a script to see how many assets have ids

---
+++ Overstock/elasticsearch/has-id.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# See how many assets have ids 
+
+INDEX=${1:-adp-assets}
+DEFINITION=${2:-has-id.json}
+PORT=9200
+CMD="curl --silent \
+	  --show-error \
+	  --request GET \
+	  --header Content-Type:application/json \
+          --data @${DEFINITION}  \
+	  http://localhost:${PORT}/${INDEX}/asset/_search"
+#echo ${CMD}
+${CMD} | python -m json.tool
