6a3e6547b4baba5c33b4d340f7433ce05cf2debb
Ron Kurr <kurr@jvmguy.com>
2019-12-11 17:17:31 +0000

    Added someting to detect missing field.

---
+++ Overstock/elasticsearch/missing-id.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# See how many assets have ids 
+
+INDEX=${1:-adp-assets}
+DEFINITION=${2:-missing-id.json}
+PORT=9200
+CMD="curl --silent \
+	  --show-error \
+	  --request GET \
+	  --header Content-Type:application/json \
+          --data @${DEFINITION}  \
+	  http://localhost:${PORT}/${INDEX}/asset/_search?pretty=true"
+#echo ${CMD}
+#${CMD} | python -m json.tool
+${CMD}
