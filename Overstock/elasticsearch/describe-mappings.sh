89662c495e57b652856cee4039433d2933154505
Ron Kurr <kurr@jvmguy.com>
2020-02-11 13:04:47 -0500

    Use cURL instead of http

--- Overstock/elasticsearch/describe-mappings.sh
+++ Overstock/elasticsearch/describe-mappings.sh
@@ -3,8 +3,9 @@
 # pull down the Elasticsearch mapping from an index
 
 PORT=${1:-9200}
-INDEX=${2:-ronbo}
+INDEX=${2:-adp-assets}
 TYPE=${3:-asset}
-CMD="http localhost:${PORT}/${INDEX}/${TYPE}/_mapping"
-echo ${CMD}
-${CMD}
+#CMD="http localhost:${PORT}/${INDEX}/${TYPE}/_mapping"
+CMD="curl --silent http://localhost:${PORT}/${INDEX}/${TYPE}/_mapping?pretty=true"
+#echo ${CMD}
+${CMD} | less
