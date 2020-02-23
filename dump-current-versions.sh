ac20630748cb746f313dc094b21bc56a5ab551d9
Ron Kurr <kurr@kurron.org>
2020-02-23 11:27:50 -0500

    Remove unsupported version checks

--- dump-current-versions.sh
+++ dump-current-versions.sh
@@ -14,8 +14,8 @@ echo "* Upgraded: ${PYTHON}"
 PYTHON_LEGACY=$(python --version)
 echo "* Upgraded: Legacy ${PYTHON_LEGACY}"
 
-VIRTUALENV=$(virtualenv --version)
-echo "* Upgraded: Virtualenv ${VIRTUALENV}"
+#VIRTUALENV=$(virtualenv --version)
+#echo "* Upgraded: Virtualenv ${VIRTUALENV}"
 
 DOCKER=$(docker --version)
 echo "* Upgraded: ${DOCKER}"
@@ -32,20 +32,20 @@ echo "* Upgraded: ${AWS}"
 ECS=$(ecs-cli --version)
 echo "* Upgraded: AWS ${ECS}"
 
-KUBE=$(kubectl version --short=true --client=true)
-echo "* Upgraded: Kubectl ${KUBE}"
+#KUBE=$(kubectl version --short=true --client=true)
+#echo "* Upgraded: Kubectl ${KUBE}"
 
-HELM="$(helm version --client --short)"
-echo "* Upgraded: Helm ${HELM}"
+#HELM="$(helm version --client --short)"
+#echo "* Upgraded: Helm ${HELM}"
 
-LOCALSTACK=$(localstack --version)
-echo "* Upgraded: localstack ${LOCALSTACK}"
+#LOCALSTACK=$(localstack --version)
+#echo "* Upgraded: localstack ${LOCALSTACK}"
 
-SYSDIG=$(sysdig --version)
-echo "* Upgraded: ${SYSDIG}"
+#SYSDIG=$(sysdig --version)
+#echo "* Upgraded: ${SYSDIG}"
 
-FALCO=$(falco --version)
-echo "* Upgraded: ${FALCO}"
+#FALCO=$(falco --version)
+#echo "* Upgraded: ${FALCO}"
 
 NOMAD=$(nomad --version)
 echo "* Upgraded: ${NOMAD}"
@@ -59,28 +59,28 @@ echo "* Upgraded: ${TERRAFORM}"
 VAULT=$(vault --version)
 echo "* Upgraded: ${VAULT}"
 
-NODE=$(node --version)
-echo "* Upgraded: NodeJS ${NODE}"
+#NODE=$(node --version)
+#echo "* Upgraded: NodeJS ${NODE}"
 
-NPM=$(npm --version)
-echo "* Upgraded: NPM ${NPM}"
+#NPM=$(npm --version)
+#echo "* Upgraded: NPM ${NPM}"
 
-YO=$(yo --version)
-echo "* Upgraded: Yeoman ${YO}"
+#YO=$(yo --version)
+#echo "* Upgraded: Yeoman ${YO}"
 
-BOWER=$(bower --version)
-echo "* Upgraded: Bower ${BOWER}"
+#BOWER=$(bower --version)
+#echo "* Upgraded: Bower ${BOWER}"
 
-GULP=$(gulp --version)
-echo "* Upgraded: Gulp ${GULP}"
+#GULP=$(gulp --version)
+#echo "* Upgraded: Gulp ${GULP}"
 
-GRUNT=$(grunt --version)
-echo "* Upgraded: Grunt ${GRUNT}"
+#GRUNT=$(grunt --version)
+#echo "* Upgraded: Grunt ${GRUNT}"
 
-SERVERLESS=$(serverless --version)
-echo "* Upgraded: Serverless ${SERVERLESS}"
+#SERVERLESS=$(serverless --version)
+#echo "* Upgraded: Serverless ${SERVERLESS}"
 
-echo "* Upgraded: IntelliJ IDEA 0"
-echo "* Upgraded: PyCharm 0"
+#echo "* Upgraded: IntelliJ IDEA 0"
+#echo "* Upgraded: PyCharm 0"
 echo "* Upgraded: Support for VirtualBox 0"
-echo "* Upgraded: Support for VMWare Fusion 0"
+#echo "* Upgraded: Support for VMWare Fusion 0"
