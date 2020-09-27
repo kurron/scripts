1d0d929d686b2c97bb6bcdd44197a67ae04e9e44
Ron Kurr <kurr@kurron.org>
2020-09-27 16:31:34 -0400

    Not sure why I did this

--- install-dvd-playback.sh
+++ install-dvd-playback.sh
@@ -2,13 +2,13 @@
 
 # https://itsfoss.com/play-dvd-ubuntu-1310/ 
 
-sudo apt install libdvd-pkg
-sudo dpkg-reconfigure libdvd-pkg
-sudo apt-get install vlc
+#sudo apt install libdvd-pkg
+#sudo dpkg-reconfigure libdvd-pkg
+#sudo apt-get install vlc
 
 curl --output /tmp/libdvdcss2.deb http://download.videolan.org/pub/debian/stable/libdvdcss2_1.2.13-0_amd64.deb 
 sudo dpkg -i /tmp/libdvdcss2.deb
 sudo apt-get install -f
 
-sudo apt-get install libdvdread4 libdvdnav4
+#sudo apt-get install libdvdread4 libdvdnav4
  
