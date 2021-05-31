1db8c8f556603c3c366455e2ede20249b4b9b698
Ron Kurr <kurr@kurron.org>
2021-05-30 23:16:01 -0400

    Minor Packer fixes.

--- install-packer.sh
+++ install-packer.sh
@@ -10,4 +10,5 @@ error_exit()
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - || error_exit "Unable to get key."
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" || error_exit "Unable to add repository."
 sudo apt-get update && sudo apt-get install packer || error_exit "Unable to install Packer."
+sudo apt-get install libarchive-tools || error_exit "Unable to install bdtar."
 
