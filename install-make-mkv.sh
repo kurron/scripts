828c4b36eda7704ba7a2804e739d5d5d491b5ffa
Ron Kurr <kurr@kurron.org>
2022-04-02 09:59:13 -0400

    Version bump to current release

--- install-make-mkv.sh
+++ install-make-mkv.sh
@@ -7,7 +7,7 @@ error_exit()
           exit 1
 }
 
-VERSION=1.16.5
+VERSION=1.16.7
 BINARY="http://www.makemkv.com/download/makemkv-bin-${VERSION}.tar.gz"
 SOURCE="http://www.makemkv.com/download/makemkv-oss-${VERSION}.tar.gz"
 PACKAGES="sudo apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev curl"
