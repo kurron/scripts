34966c5d8597f9fdd8d372e82f68ed337c982978
Ron Kurr <kurr@kurron.org>
2023-01-14 15:38:53 -0500

    Update Brave installation

--- install-brave.sh
+++ install-brave.sh
@@ -9,10 +9,12 @@ error_exit()
           exit 1
 }
 
-sudo apt install apt-transport-https curl || error_exit "Unable to install necessary programs."
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - || error_exit "Unable to install repository key."
-source /etc/os-release || error_exit "Unable to determine OS."
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list || error_exit "Unable to repository."
-sudo apt update || error_exit "Unable to determine OS."
-sudo apt install brave-browser || error_exit "Unable to install browser."
+#sudo apt install apt-transport-https curl || error_exit "Unable to install necessary programs."
+#curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - || error_exit "Unable to install repository key."
+#source /etc/os-release || error_exit "Unable to determine OS."
+#echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list || error_exit "Unable to repository."
+#sudo apt update || error_exit "Unable to determine OS."
+#sudo apt install brave-browser || error_exit "Unable to install browser."
 
+# from https://brave.com/linux/
+sudo apt install curl && sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list && sudo apt update && sudo apt install brave-browser
