4714f2b16a257cd2ffc04bf2559a6d9def5bdb47
Ron Kurr <kurr@kurron.org>
2023-04-02 16:26:42 -0400

    Upgrade to current release

--- install-docker.sh
+++ install-docker.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+# https://docs.docker.com/engine/install/ubuntu/
 
 sudo apt-get update
 
 sudo apt-get install \
-    apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo mkdir -p /etc/apt/keyrings
 
-sudo apt-key fingerprint 0EBFCD88
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
- sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 docker --version
 
