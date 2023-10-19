8de9254f341d17e298c8437e7ae90c73814defbd
Ron Kurr <kurr@kurron.org>
2023-10-19 18:21:20 -0400

    Upgrade to current release

--- install-make-mkv.sh
+++ install-make-mkv.sh
@@ -7,18 +7,18 @@ error_exit()
           exit 1
 }
 
-VERSION=1.17.3
-BINARY="http://www.makemkv.com/download/makemkv-bin-${VERSION}.tar.gz"
-SOURCE="http://www.makemkv.com/download/makemkv-oss-${VERSION}.tar.gz"
+VERSION=1.17.5
+BINARY="https://www.makemkv.com/download/makemkv-bin-${VERSION}.tar.gz"
+SOURCE="https://www.makemkv.com/download/makemkv-oss-${VERSION}.tar.gz"
 FFMPEG="https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2"
 PACKAGES="sudo apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev curl yasm libfdk-aac-dev libmp3lame-dev"
 
 echo ${PACKAGES}
 ${PACKAGES} || error_exit "Unable to install required packages."
 
-GET_BINARY="curl --output /tmp/makemkv-bin.tar.gz ${BINARY}" 
-GET_SOURCE="curl --output /tmp/makemkv-oss.tar.gz ${SOURCE}"
-GET_FFMPEG="curl --output /tmp/ffmpeg-snapshot.tar.bz2 ${FFMPEG}"
+GET_BINARY="curl --verbose --output /tmp/makemkv-bin.tar.gz ${BINARY}" 
+GET_SOURCE="curl --verbose --output /tmp/makemkv-oss.tar.gz ${SOURCE}"
+GET_FFMPEG="curl --verbose --output /tmp/ffmpeg-snapshot.tar.bz2 ${FFMPEG}"
 
 echo ${GET_BINARY}
 ${GET_BINARY}  || error_exit "Unable to get binaries."
@@ -29,6 +29,11 @@ ${GET_SOURCE}  || error_exit "Unable to install source files."
 echo ${GET_FFMPEG}
 ${GET_FFMPEG}  || error_exit "Unable to install FFMPEG files."
 
+echo Compiling FFMPEG 
 cd /tmp && tar xvf /tmp/ffmpeg-snapshot.tar.bz2 && cd /tmp/ffmpeg && ./configure --prefix=/tmp/ffmpeg --enable-static --disable-shared --enable-pic --enable-libfdk-aac && make && sudo make install
+
+echo Compiling MakeMKV OSS
 cd /tmp && tar zxvf /tmp/makemkv-oss.tar.gz && cd /tmp/makemkv-oss-${VERSION} && PKG_CONFIG_PATH=/tmp/ffmpeg/lib/pkgconfig ./configure && make && sudo make install
+
+echo Compiling MakeMKV bin 
 cd /tmp && tar zxvf /tmp/makemkv-bin.tar.gz && cd /tmp/makemkv-bin-${VERSION} && make && sudo make install
